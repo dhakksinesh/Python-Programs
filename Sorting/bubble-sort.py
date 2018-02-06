53361cd9ab69b10627add0c744f8efa77776a358
Dhakksinesh <93026789+dhakksinesh@users.noreply.github.com>
2018-02-06 14:10:09 +0530

    bubble sort

---
+++ Sorting/bubble-sort.py
@@ -0,0 +1,11 @@
+def bubble_sort(list1):   
+    for i in range(0,len(list1)-1):  
+        for j in range(len(list1)-1):  
+            if(list1[j]>list1[j+1]):  
+                temp = list1[j]  
+                list1[j] = list1[j+1]  
+                list1[j+1] = temp  
+    return list1  
+list1 = [7, 4, 5, 1, 3, 6]  
+print("The unsorted list is: ", list1)  
+print("The sorted list is: ", bubble_sort(list1))  
